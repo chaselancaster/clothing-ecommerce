a2ce8d55a80f8a88892e99f9edc35dd1bf501167
chaselancaster <chaseblancaster@gmail.com>
2019-12-21 13:58:21 -0800

    pass title and items as props in collection-preview component. map over those items and add title to classname

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 import "./collection-preview.styles.scss";
 
-const CollectionPreview = () => (
+const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
-    <h1 className="title">Title</h1>
-    <div className="preview"></div>
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items.map(item => (
+        <div>{item.name}</div>
+      ))}
+    </div>
   </div>
 );
