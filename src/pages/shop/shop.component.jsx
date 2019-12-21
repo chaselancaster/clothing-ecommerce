2c223ddece2b65e04179b99b719653e118cbc918
chaselancaster <chaseblancaster@gmail.com>
2019-12-21 14:00:27 -0800

    import CollectionPreview in shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import SHOP_DATA from "./shop.data.js";
 
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
