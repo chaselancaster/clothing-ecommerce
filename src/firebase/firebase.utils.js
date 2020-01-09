ebfea8982aefb5647afa71db1c2a63cc6c697195
Chase Lancaster <chaselancaster@Chases-MacBook-Pro.local>
2020-01-08 19:18:21 -0800

    add config object for firebase and use firebase.initializeApp(config)

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,3 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyDUSqqXilKNGnsg-OCVlW4nnT-ySF3O6J0",
+    authDomain: "ecommerce-39692.firebaseapp.com",
+    databaseURL: "https://ecommerce-39692.firebaseio.com",
+    projectId: "ecommerce-39692",
+    storageBucket: "ecommerce-39692.appspot.com",
+    messagingSenderId: "833025485378",
+    appId: "1:833025485378:web:2e7cd8346276ce1a00dbdb",
+    measurementId: "G-E3MS2QTQWD"
+  };
+
+  firebase.initializeApp(config);
