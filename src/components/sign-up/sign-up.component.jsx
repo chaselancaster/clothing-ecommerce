96d50660f29f7509483b3220c07cd169e6a2a4a8
chaselancaster <chaseblancaster@gmail.com>
2020-01-15 16:57:46 -0800

    add constructor, super, and state to sign-up component

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -7,3 +7,15 @@ import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 
 import './sign-up.styles.scss';
 
+class SignUp extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            displayName: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
+}
