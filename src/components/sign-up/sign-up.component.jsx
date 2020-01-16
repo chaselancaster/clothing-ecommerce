a0e8ea60d9efbf20fd6cba53ff8c7c85566ca3c6
chaselancaster <chaseblancaster@gmail.com>
2020-01-15 17:03:29 -0800

    add sign-up forms with 4 FormInput fields

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -18,4 +18,21 @@ class SignUp extends React.Component {
             confirmPassword: ''
         }
     }
+
+
+    render () {
+        const { displayName, email, password, confirmPassword} = this.state;
+        return (
+            <div className="sign-up">
+                <h2 className="title">I do not have an account</h2>
+                <span>Sign up with your email and password</span>
+                <form className="sign-up-form" onSubmit={this.handleSubmit}>
+                    <FormInput type="text" name="displayName" value={displayName} onChage={this.handleChange} label="Display Name" required></FormInput>
+                    <FormInput type="email" name="email" value={email} onChage={this.handleChange} label="Email" required></FormInput>
+                    <FormInput type="password" name="password" value={password} onChage={this.handleChange} label="Password" required></FormInput>
+                    <FormInput type="password" name="confirmPassword" value={confirmPassword} onChage={this.handleChange} label="Confirm Password" required></FormInput>
+                </form>
+            </div>
+        )
+    }
 }
