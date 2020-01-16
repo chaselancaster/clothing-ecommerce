c8b2633324565a736f3de5b210770f3771146547
chaselancaster <chaseblancaster@gmail.com>
2020-01-15 17:04:31 -0800

    add CustomButton to sign-up component

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -27,10 +27,11 @@ class SignUp extends React.Component {
                 <h2 className="title">I do not have an account</h2>
                 <span>Sign up with your email and password</span>
                 <form className="sign-up-form" onSubmit={this.handleSubmit}>
-                    <FormInput type="text" name="displayName" value={displayName} onChage={this.handleChange} label="Display Name" required></FormInput>
-                    <FormInput type="email" name="email" value={email} onChage={this.handleChange} label="Email" required></FormInput>
-                    <FormInput type="password" name="password" value={password} onChage={this.handleChange} label="Password" required></FormInput>
-                    <FormInput type="password" name="confirmPassword" value={confirmPassword} onChage={this.handleChange} label="Confirm Password" required></FormInput>
+                    <FormInput type="text" name="displayName" value={displayName} onChage={this.handleChange} label="Display Name" required/>
+                    <FormInput type="email" name="email" value={email} onChage={this.handleChange} label="Email" required/>
+                    <FormInput type="password" name="password" value={password} onChage={this.handleChange} label="Password" required/>
+                    <FormInput type="password" name="confirmPassword" value={confirmPassword} onChage={this.handleChange} label="Confirm Password" required/>
+                    <CustomButton type="submit">SIGN UP</CustomButton>
                 </form>
             </div>
         )
