543ede0f5921d73670c711607d2d4725a7271eb1
chaselancaster <chaseblancaster@gmail.com>
2020-01-22 11:15:56 -0800

    create mapStateToProps function and attach it to connect in header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -30,4 +30,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
