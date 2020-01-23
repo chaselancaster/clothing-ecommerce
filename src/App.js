0f0091580e253b041b50cc26e0cc35737415f547
chaselancaster <chaseblancaster@gmail.com>
2020-01-23 10:30:04 -0800

    add mapDispatchToProps function in App.js and grab setCurrentUser from user.actions under redux

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } } from './redux/user/user.actions';
 
 class App extends React.Component {
   constructor() {
@@ -58,4 +59,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
